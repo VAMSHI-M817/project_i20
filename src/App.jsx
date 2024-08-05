f801499820e63cdf66ac1599cbdd07b4e50362b7
VAMSHI-M817 <vamshimarepu817@gmail.com>
2024-08-05 23:03:23 +0530

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,24 @@
-import { useState } from 'react'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  console.time("Time")
+  function bubbleSort(arr = [34, 7, 23, 32, 5, 62, 13, 45]) {
+    let n = arr.length - 1
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < n - i; j++) {
+        if (arr[j] > arr[j + 1]) {
+          let temp = arr[j]
+          arr[j] = arr[j + 1]
+          arr[j + 1] = temp
+        }
+      }
+    }
+    return arr
+  }
+  console.log(bubbleSort());
+  console.timeEnd("Time")
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
-
-  return (
-    <>
-      <h1>Hey Vamshi Im working</h1>
-    </>
-  )
+  return (<></>)
 }
 
 export default App
